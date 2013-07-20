f4aa96928a4551120c81348b686a447afa55e47b
Andrey Hoursanov <andrey.hoursanov@sap.com>
2013-07-20 10:44:22 +0000

    My 1st responsive bitstarter

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
+#!/usr/bin node
+
 var express = require('express');
 var fs = require('fs');
 var htmlfile = 'index.html';
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = express(express.logger());
 
 app.get('/', function(request, response) {
   // 2013.07.17: response.send(fs.readFileSync(htmlfile,'utf8'));
